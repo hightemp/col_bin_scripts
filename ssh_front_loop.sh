6281dd0e9d495cc17ce0f3a35db390d898f70ffc
Антон Панов <a.panov@avtoexpress.org>
2022-09-25 09:04:48 +0300

    update

--- ssh_front_loop.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-echo `date` "START"
-while true ; do
-	ssh VM-FRONT;
-done
--- ssh_front_ps.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-echo `date` "START"
-while true ; do
-	ssh -t VM-FRONT 'watch ps aux --sort=-pcpu';
-done
